1a279e42c156ab86de7006fbe6a1e585516fea9d
Yoshihiro Tanaka <contact@cordea.jp>
2016-04-10 16:27:59 +0900

    fixed date format

--- app/src/main/java/jp/cordea/switter/MainListAdapter.java
+++ app/src/main/java/jp/cordea/switter/MainListAdapter.java
@@ -5,13 +5,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.twitter.sdk.android.core.models.Tweet;
 
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
+import org.joda.time.Interval;
+import org.joda.time.format.DateTimeFormat;
+
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Yoshihiro Tanaka on 16/03/30.
@@ -44,7 +49,6 @@ public class MainListAdapter extends ArrayAdapter<Tweet> {
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = convertView == null ? LayoutInflater.from(getContext()).inflate(R.layout.main_list_item, null, false) : convertView;
 
-
         TextView nameTextView = (TextView) view.findViewById(R.id.user_name);
         TextView idTextView = (TextView) view.findViewById(R.id.user_id);
         TextView dateTextView = (TextView) view.findViewById(R.id.date);
@@ -56,11 +60,29 @@ public class MainListAdapter extends ArrayAdapter<Tweet> {
         ImageView replyButton = (ImageView) view.findViewById(R.id.reply_button);
 
         final Tweet tweet = tweets.get(position);
-        nameTextView.setText(tweet.user.screenName);
-        idTextView.setText(tweet.user.idStr);
-        dateTextView.setText(tweet.createdAt);
+        nameTextView.setText(tweet.user.name);
+        idTextView.setText(tweet.user.screenName);
+        DateTime time = parseTwitterDate(tweet.createdAt);
+        DateTime now = new DateTime();
+        Duration duration = new Interval(time, now).toDuration();
+        long display = duration.getStandardMinutes();
+        int format = R.string.minute_format;
+        if (display >= 60) {
+            format = R.string.hour_format;
+            display = duration.getStandardHours();
+            if (display >= 60) {
+                format = R.string.day_format;
+                display = duration.getStandardDays();
+            }
+        }
+
+        dateTextView.setText(String.format(getContext().getResources().getString(format), display));
         contentTextView.setText(tweet.text);
 
         return view;
+    }
+
+    private DateTime parseTwitterDate(String twitterDate) {
+        return DateTime.parse(twitterDate, DateTimeFormat.forPattern("EEE MMM dd HH:mm:ss Z yyyy").withLocale(Locale.ENGLISH));
     }
 }
