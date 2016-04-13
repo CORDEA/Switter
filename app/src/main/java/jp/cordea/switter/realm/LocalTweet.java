ddcc8a48adf4c5b1ba8f773e521dc7f1891e908c
Yoshihiro Tanaka <contact@cordea.jp>
2016-04-13 09:33:26 +0900

    renamed model: Tweet to LocalTweet

--- app/src/main/java/jp/cordea/switter/realm/LocalTweet.java
+++ app/src/main/java/jp/cordea/switter/realm/LocalTweet.java
@@ -3,16 +3,28 @@ package jp.cordea.switter.realm;
 import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
 import io.realm.annotations.Required;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Created by Yoshihiro Tanaka on 16/04/05.
  */
-public class Tweet extends RealmObject {
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class LocalTweet extends RealmObject {
     @PrimaryKey
     private long epoch;
 
     @Required
     private String text;
+
     private String createdAt;
+    private String name;
+    private String screenName;
+    private String profileImageUrl;
+
     private boolean isRetweet;
+    private boolean isReply;
 }
